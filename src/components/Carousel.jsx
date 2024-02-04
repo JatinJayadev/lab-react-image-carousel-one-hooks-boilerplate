6f003dce4bf40c2fea5de8bb944993899b9457c9
Jatin Jayadev <jatin.batchu@kalvium.community>
2024-02-04 17:29:36 +0530

    Added

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -8,6 +8,50 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // complete the function below:
 function Carousel() {
+
+    const [state, setState] = useState(0)
+    const data = images[state]
+
+    const handleBackClick = () => {
+        if (state == 0) {
+            setState(2)
+        }
+        else {
+            setState(state - 1)
+        }
+    }
+
+    const handleForwardClick = () => {
+        if (state == 2) {
+            setState(0)
+        }
+        else {
+            setState(state + 1)
+        }
+    }
+
+    return (
+
+        <div className="container">
+
+            <ArrowBackIosIcon className="arrows" onClick={handleBackClick} />
+
+            <div className="details" >
+                <div>
+                    <h2>{data.title}</h2>
+                </div>
+                <div>
+                    <h3>{data.subtitle}</h3>
+                </div>
+
+            </div>
+
+            <img src={data.img} alt="" />
+
+            <ArrowForwardIosIcon className="arrows" onClick={handleForwardClick} />
+
+        </div>
+    )
 }
 
 export default Carousel;
